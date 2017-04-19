7a81e9a2dfa646fe8140e924243a5272482c47af
Ram Bhat Mulky <rambhatm@yahoo.com>
2017-04-19 19:03:32 +0530

    geojson indexing

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ var express = require('express')
 var body_parser = require('body-parser')
 var mongo_client = require('mongodb').MongoClient
 
+//Connect to DB and index it for geospatial query
 
 var db
-
 mongo_client.connect('mongodb://ds_admin:ds_password@ds163940.mlab.com:63940/ds_message', (err, database) => {
     if (err) return console.log('DB connect error '+err)
     db = database
+    //should createIndex be here?
+    db.collection('messages').createIndex({ loc : "2dsphere"})
     console.log('DB connected') 
 })
+//Create APP and ROUTER
 
 var app = express()
 app.use(body_parser.urlencoded({extended : true}))
@@ -17,19 +20,25 @@ app.use(body_parser.json())
 
 var router = express.Router()
 
+//MIDDLEWARE
+
 router.use((req, res, next) => {
     console.log(req.method + ' request on url : ' + req.url)
     next()
 })
-/*
-    API ENDPOINTS
-    URL : http://URL/api/messages
-*/
+
+
+//API ENDPOINTS
+//URL : http://URL/api/messages
+
 router.route('/messages')
+
     .post((req, res) => {
         var new_message = {
-            lat : req.body.lat,
-            long : req.body.long,
+            loc : {
+                type : "Point",
+                coordinates : [req.body.long, req.body.lat]
+            },
             msg : req.body.msg
         }
         db.collection('messages').save(new_message, (err, result) => {
@@ -38,6 +47,7 @@ router.route('/messages')
             res.end()
         })
     })
+
     .get((req, res) => {
         var lat = req.query.lat
         var long = req.query.long

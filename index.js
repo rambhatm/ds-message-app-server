d9083c7e356cda2f88e3e64664ad3ef6fb7b3331
Ram Bhat Mulky <rambhatm@yahoo.com>
2017-04-19 16:47:28 +0530

    working post, db

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var express = require('express')
 var body_parser = require('body-parser')
+var mongo_client = require('mongodb').MongoClient
+
+
+var db
+
+mongo_client.connect('mongodb://ds_admin:ds_password@ds163940.mlab.com:63940/ds_message', (err, database) => {
+    if (err) return console.log('DB connect error '+err)
+    db = database
+    console.log('DB connected') 
+})
 
 var app = express()
 app.use(body_parser.urlencoded({extended : true}))
@@ -8,24 +18,33 @@ app.use(body_parser.json())
 var router = express.Router()
 
 router.use((req, res, next) => {
-    console.log(req.method + " request on url : " + req.url)
+    console.log(req.method + ' request on url : ' + req.url)
     next()
 })
 /*
     API ENDPOINTS
     URL : http://URL/api/messages
-
-
-
 */
 router.route('/messages')
     .post((req, res) => {
-        var lat = req.body.lat
-        var long = req.body.long
+        var new_message = {
+            lat : req.body.lat,
+            long : req.body.long,
+            msg : req.body.msg
+        }
+        db.collection('messages').save(new_message, (err, result) => {
+            if (err) return console.log('DB save error POST')
+            console.log('DB save done..' + new_message)
+            res.end()
+        })
     })
     .get((req, res) => {
+        var lat = req.query.lat
+        var long = req.query.long
         res.json({
-            message : "Goodbye money"
+            message : "Goodbye money",
+            lat : lat,
+            long : long
         })
     })
 
